1b8a201dc9ed81be757dc44dfb401af5b5d96842
inhyeok <ss112d86@gmail.com>
2022-03-17 02:23:37 +0900

    Feat: Docker 테스트 환경 구축

--- src/test/java/com/upgle/api/domain/user/UserRepositoryTest.java
+++ src/test/java/com/upgle/api/domain/user/UserRepositoryTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
 
